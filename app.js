0d520239b1118d660cdb8172578e787ed9edd563
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2023-01-10 21:10:25 +0500

    Counting Number of 1's in an array

--- app.js
+++ app.js
@@ -106,6 +106,18 @@
 
 // }
 
+// var arr = [0, null, undefined, 1, -1, false, true, [], {}, ''];
 
+//  Counting Number of 1's in an array
+var arr = [1,0,0,1,1,1,1,1,1,1,1,0,0,0,0]
+var count = 0;
+
+for (let i = 0; i < arr.length; i++){
+    if (arr[i] == 1){
+        count ++;
+    }
+};
+
+console.log(count);
 
 
