020bce25e513adde5732cdb8a6889589a1b11d24
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2023-01-10 20:28:44 +0500

    Creating a method function searchThroughObject()

--- app.js
+++ app.js
@@ -98,28 +98,25 @@ var a = {
         },
     };
 
-// console.log(isObject(a));
-// // console.log(typeof a); // object
-
-// // for (const [key, value] of Object.entries(givenObject)) {
-// //     console.log(`${key}: ${value}`);
-// // };
-
-// function isObject(objValue) {
-//     return objValue && typeof objValue === 'object' && objValue.constructor === Object;
-// }
-
-const object = { a: 1, b: 2, c: 3 };
-
-// for (const property in object) {
-//     console.log(`${property}: ${object[property]}`);
-// }
-
 // Checking if the object contains a value or not
 // Object.values return an array of values of the object .includes() checks if the array contains the value or not
-if (Object.values(object).includes(3)){
-    console.log('yes');
-}
-else {
-    console.log('no');
+
+// const object = { a: 1, b: 2, c: 3 };
+
+// if (Object.values(object).includes(3)){
+//     console.log('yes');
+// }
+// else {
+//     console.log('no');
+// }
+
+
+var brand = document.getElementById("brand");
+console.log(brand);
+var model = document.getElementById("model");
+console.log(model);
+
+function searchThroughObject(array_object){
+    var obj = a.mobiles[brand.value][model.value];
+    return obj;
 }
