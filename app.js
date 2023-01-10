c5ad21a33fae0545d21fba072d54a89f35b97a29
Muhammad Haroon Zafar <mr.haroon.zafar@gmail.com>
2023-01-10 19:47:57 +0500

    Taking Inputs of brand and model, and output object.

---
+++ app.js
@@ -0,0 +1,106 @@
+// two inputs 
+// one for model 
+// one for brand
+
+// For Input 
+
+var modelInput = prompt("Enter Model");
+var brandInput = prompt("Enter Brand");
+
+// For Output
+var a = {
+    mobiles: {
+        samsung: {
+        a30: {
+            name: "a30",
+            ram: "4gb",
+            rom: "64gb",
+            camera: "50px",
+            price: 2000,
+        },
+        note10: {
+            name: "note10",
+            ram: "6gb",
+            rom: "128gb",
+            camera: "50px",
+            price: 4000,
+            },
+        s10: {
+            name: "s10",
+            ram: "3gb",
+            rom: "128gb",
+            camera: "10px",
+            price: 5000,
+            },
+        },
+        iphone: {
+        iphone4: {
+            name: "iphone4",
+            ram: "4gb",
+            rom: "64gb",
+            camera: "50px",
+            price: 10000,
+            },
+            iphone4s: {
+            name: "iphone4s",
+            ram: "4gb",
+            rom: "64gb",
+            camera: "50px",
+            price: 20000,
+            },
+            iphone5: {
+            name: "iphone5",
+            ram: "4gb",
+            rom: "64gb",
+            camera: "50px",
+            price: 30000,
+            },
+            iphone6: {
+            name: "iphone6",
+            ram: "4gb",
+            rom: "64gb",
+            camera: "50px",
+            price: 30000,
+            },
+        },
+        xiomi: {
+            redminote10: {
+            name: "redminote10",
+            ram: "4gb",
+            rom: "64gb",
+            camera: "50px",
+            price: 10000,
+            },
+            redminote11: {
+            name: "redminote11",
+            ram: "4gb",
+            rom: "64gb",
+            camera: "50px",
+            price: 10000,
+            },
+            redmi10: {
+            name: "redmi10",
+            ram: "4gb",
+            rom: "64gb",
+            camera: "50px",
+            price: 10000,
+            },
+            redmi10pro: {
+            name: "redmi10pro",
+            ram: "4gb",
+            rom: "64gb",
+            camera: "50px",
+            price: 10000,
+            },
+        },
+        oppo: {
+            a3s: {
+            name: "a3s",
+            ram: "4gb",
+            rom: "64gb",
+            camera: "50px",
+            price: 10000,
+            },
+        },
+        },
+    };
